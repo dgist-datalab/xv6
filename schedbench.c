1a57950546813612ad07e7055234acfbc4b13c02
Juhyung Park <qkrwngud825@gmail.com>
2024-10-10 15:24:16 +0900

    Add schedbench
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

---
+++ schedbench.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  // TODO: Will be provided later
+  printf(1, "Will be provided later\n");
+  exit();
+}
